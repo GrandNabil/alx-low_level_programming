e9b9ef73d8b365368ca5fc82fe4ddde20b3c78fb
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-10 04:22:09 -0800

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /**
- * main - prints the phrase "Programming is like building multilingual puzzle
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
+
+./betty-style.pl main.c
+
 int main (void) 
 {
-	puts ("\"Programming is like building multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	
 	return 0;
 }
