0507c7fdc6e1916e30ba97d65bbc0d610f39a0b6
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-10 05:57:10 -0800

    programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art
+ * programm return 0 if exited proprly
+ */
+int main(void)
+{
+	printf("with proper grammar, butthe outcome is a pieceof art,\n");
+	return(0);
+}
