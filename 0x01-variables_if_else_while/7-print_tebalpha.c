fc53fc8fadaf1c919512c186cce0f03baf638968
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-13 15:03:11 -0700

    programming

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lower case alphabet in reserve,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char chr;
+
+	for (chr = 'z'; chr >= 'a'; chr--)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+	return (0);
