daf1990b37bbc29bc0daf32230d2a0b3bda0ede5
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-13 00:56:25 -0800

    programming

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,9 @@ int main(void)
 	int alph;
 
 	for (alph = 'a'; alph <= 'z'; alph++)
-	{
 		putchar(alph);
-	}
 	for (alph = 'A'; alph <= 'Z'; alph++)
-	{
 		putchar(alph);
-	}
 	putchar('\n');
 	return (0);
 }
