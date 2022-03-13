8ae4669ff30741147e42a22372942d76b8dd4918
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-13 01:08:49 -0800

    programming

---
+++ 0x01-variables_if_else_while/4-print_alphbt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lower case,
+ * followed by a new line, except q and e
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		if (alph != 'e' && alph != 'a')
+		{
+			putchar(alph);
+		}
+		alph++;
+	}
+	putchar('\n');
+	return(0);
+}
