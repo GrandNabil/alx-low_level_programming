dadeb7e475abee3a36f0f84b9ba83a503ee22992
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-15 09:35:44 -0700

    Functions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the lowercase to check
+ * Return: 1 if c is the lowercase, 0 otherwise
+ */
+int islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
