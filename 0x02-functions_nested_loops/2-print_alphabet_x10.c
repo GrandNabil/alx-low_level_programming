eaf8546a41d9f05d87f1ca507e7d93b03a882901
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-15 08:27:12 -0700

    Functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase;
+ * followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char alph;
+
+	for (i < 0; i = 10; i++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+		_putchar(ch);
+		}
+	_putchar('\n');
+	}
+}	
