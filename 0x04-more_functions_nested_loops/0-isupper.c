4c1b8192dccc12452ccb2ec52c3c28cb2b22eeb6
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-17 05:33:07 -0700

    more functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * _isupper - checks for uppercase character
  * @c: int to be checked
- * Return:  1 if c is digit 0 otherwse
+ * Return: 1 if c is a digit 0 otherwise
  */
 
 int _isupper(int c)
 {
-	return(c >= 'A' && c <= 'Z');
+	return (c >= 'A' && c <= 'Z');
 }
