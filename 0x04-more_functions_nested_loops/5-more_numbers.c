492ba7ba9cddef9a6921cf68a614f2b8e21d2a56
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-17 14:33:23 -0700

    more function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers from 0 to 14
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
  */
-
 void more_numbers(void)
 {
 	int n, m;
@@ -14,9 +13,7 @@ void more_numbers(void)
 		for (m = 0; m < 15; m++)
 		{
 			if (m >= 10)
-			{
 				_putchar(m / 10 + '0');
-			}
 			_putchar(m % 10 + '0');
 		}
 		_putchar('\n');
