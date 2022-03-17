39ce83fb1c736b521a5f7811b9ee3ac6697c999e
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-17 04:59:25 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: int to be checked
+ * Return:  1 if c is digit 0 otherwse
+ */
+
+int _isupper(int c);
+{
+	return(c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * Return: 1 if c is a digit
+ * 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	int c;
+
+	if (c >= 0 && c =< 9)
+	{
+		_putchar(1);
+	}
+	else {
+		_putchar(0);
+	}
+	return (0);
+}
