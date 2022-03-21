9f532e0208ccc3d759b2abc880bc07c50cf74bcc
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-21 10:15:42 -0700

    Arrays and pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 
 
 /**
- * reset_to_98(int *n) - writes a function that takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - writes a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
  * @n: pointer to the variable to update
  */
 
