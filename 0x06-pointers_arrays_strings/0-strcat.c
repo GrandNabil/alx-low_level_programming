ebc61339eb26c7ed76c91dadde939e5538e664c7
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-24 02:36:02 -0700

    more pointers arrays and string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	const char *dest = "Hello ";
+	const char *src = "World!\n";
+
+	char tab[98];
+	strcat(strcpy(buffer, dest), src);
+	_putchar(tab[98]);
+}
+
