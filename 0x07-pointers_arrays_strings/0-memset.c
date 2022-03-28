91326f4856b5e2eb86779e2535acab5f28514aba
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-03-28 03:49:55 -0700

    arrstr

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @n: bytes of the memory area pointed to
+ * @s: the memory area to fill
+ * @b: the constant byte
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0, i < n, i++)
+		s[i] = b;
+}
