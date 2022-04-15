d59675201530b13bfd25863662c08e81b9c52409
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-04-15 04:16:46 -0700

    function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FILE_function_pointers
-#define FILE_function_pointers
+#ifndef FILE_function_pointers_h
+#define FILE_function_pointers_h
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
