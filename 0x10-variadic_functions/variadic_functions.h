6292adb8db2be866429d42224408ce6e4174d7a8
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-04-15 15:42:43 -0700

    variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,16 @@
-#ifndef FILE_VARIADIC_FUNCTIONS_H
-#define FILE_VARIADIC_FUNCTIONS_H
+#ifndef VARIADIC_FUNC
+#define VARIADIC_FUNC
+
+#include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c);
-
+char *make_nil(char *s);
+int count_format(const char * const format);
+void print_comma(int j, int x);
 
 #endif
+
