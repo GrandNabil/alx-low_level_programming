1e8896d16c255c0ca0850cb1594f8478a4f4ba98
Lawani Akambi Nabil <akambilawani@gmail.com>
2022-05-03 15:40:04 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		while (text_content[s])
-			                        s++;
+			s++;
 		t = write(fd, text_content, s);
 		if (t != s)
 			return (-1);
